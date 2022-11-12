2a2e4d39528570eb0be9bd82817b0e40f22cd816
Pat Bautista <patzj006@gmail.com>
2022-11-12 09:25:58 +0800

    feat: base ipv4 syntactic validation

--- ipv4.go
+++ ipv4.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
 
 func printIpV4SubnetHeader() {
 	fmt.Println("---------------------------------")
@@ -12,6 +15,13 @@ func promptIpV4SubnetInput() {
 	ipv4 := ""
 	fmt.Print("Enter IPv4 address (e.g. 192.168.0.1): ")
 	fmt.Scanln(&ipv4)
+
+	// Syntactic validation
+	ipv4Pattern := `^(\d{1,3}\.){3}\d{1,3}$`
+	if matched, _ := regexp.MatchString(ipv4Pattern, ipv4); !matched {
+		fmt.Println("Invalid IPv4 address")
+		return
+	}
 
 	subnet := ""
 	fmt.Print("Enter subnet mask (1-32): ")
