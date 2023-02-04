4622302dce4e2feaeeaee7e3f907c3f14b1d10b3
Pat Bautista <patzj006@gmail.com>
2023-02-05 01:52:53 +0800

    remove debug print

--- utils/ipv4.go
+++ utils/ipv4.go
@@ -1,8 +1,6 @@
 package utils
 
 import (
-	"fmt"
-
 	"github.com/patzj/sinc/models"
 )
 
@@ -12,9 +10,6 @@ func Ipv4Hosts(networkAddr, broadcastAddr *models.IPv4) []models.IPv4 {
 
 	for {
 		host = models.Offset(host, 1)
-
-		fmt.Println(host.Octets().String(), broadcastAddr.Octets().String(), host.IsBefore(*broadcastAddr))
-
 		if host.IsBefore(*broadcastAddr) {
 			hosts = append(hosts, host)
 		} else {
